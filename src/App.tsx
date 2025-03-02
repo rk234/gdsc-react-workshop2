e3f03ea4e58a0c057b03435915c726feecb00b7e
Ramy Kaddouri <myramkadd@gmail.com>
2025-03-02 14:20:28 -0500

    do stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import StationInput from "./components/StationInput";
+import WeatherCard from "./components/WeatherCard"
+import { useState } from "react";
 
+const queryClient = new QueryClient();
 function App() {
+  const [activeStation, setActiveStation] = useState("")
+
+  function handleStationInput(station: string) {
+    setActiveStation(station)
+  }
 
   return (
-    <>
-      <h1> Hello World </h1>
-    </>
+
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-col w-full h-full items-center px-4 bg-gray-200">
+        <div className="max-w-2xl w-full flex flex-col gap-5">
+          <h1 className="mt-10 text-3xl font-bold">Simple Weather App</h1>
+          <StationInput onStationSelect={handleStationInput} />
+          {activeStation.length == 4 && <WeatherCard station={activeStation} />}
+        </div>
+      </div>
+
+    </QueryClientProvider>
   )
 }
 
